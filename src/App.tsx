8a534631b268a93e1c4be7cc9d1f13f764256bf5
허강현 <hgh6128@gmail.com>
2025-05-08 17:21:30 +0900

    Sidebar UI 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import OverviewPage from './pages/OverviewPage';
 import ProductsPage from './pages/ProductsPage';
+import Sidebar from './components/common/Sidebar';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <div className='absolute inset-0 backdrop:blur-sm' />
             </div>
 
+            <Sidebar />
             <Routes>
                 <Route path='/' element={<OverviewPage />} />
                 <Route path='/products' element={<ProductsPage />} />
